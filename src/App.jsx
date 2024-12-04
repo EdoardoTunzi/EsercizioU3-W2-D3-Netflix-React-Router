ffd3260cfb3643cc77ce04283d36415c4c252aab
Edoardo <edfl.tunzi@gmail.com>
2024-12-04 15:20:55 +0100

    App updated with Router, Movie details and Tvshow components created

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,24 @@ import ActionBar from "./components/ActionBar";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
 import { Container } from "react-bootstrap";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import TVShows from "./components/TVShows";
+import MovieDetails from "./components/MovieDetails";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Container fluid className="bg-dark text-light min-vh-100">
         <MyNavbar />
         <ActionBar />
-        <Main />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/tv-shows" element={<TVShows />} />
+          <Route path="/movie-details/:movieId" element={<MovieDetails />} />
+        </Routes>
         <Footer />
       </Container>
-    </>
+    </BrowserRouter>
   );
 }
 
